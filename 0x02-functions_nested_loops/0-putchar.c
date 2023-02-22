0cce10fc7100304d8f15cc649e783375fbee8602
bruneCode <102300897+josiasapelete@users.noreply.github.com>
2023-02-22 02:43:19 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - This print a string putchar
- * return: This  returns 0
+ * main - prints _putchar
+ *
+ * Return: Always 0 in success
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar(95); /*95 is asci for _*/
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
