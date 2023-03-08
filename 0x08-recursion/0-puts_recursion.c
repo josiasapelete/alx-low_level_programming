a7c16a1e175ed4527cca724cf0f497c948d00143
josiasapelete <josiasapelete@gmail.com>
2023-03-08 15:57:27 -0800

    recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *_puts_recursion - function that prints a string, followed by a new line.
+ * @s: the printed string
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
