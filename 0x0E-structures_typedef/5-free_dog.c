37210bf43030cf6f5a75824626fc9aef50a75079
josiasapelete <josiasapelete@gmail.com>
2023-03-21 18:31:23 -0700

    frees dogs done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: the dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
