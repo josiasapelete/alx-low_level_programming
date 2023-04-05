ef8063cd1ec31a9e84042188158105e92385e7a1
josiasapelete <josiasapelete@gmail.com>
2023-04-04 18:56:23 -0700

    free list part 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (*head != NULL)
+	if (*head && *head != NULL)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-	}
 
-	*head = NULL;
+		while (*head != NULL)
+		{
+			temp = *head;
+			*head = (*head)->next;
+			free(temp);
+		}
+
+		*head = NULL;
+	}
 }
