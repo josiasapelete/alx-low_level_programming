8ec0c59f64114f43ef5af65c268cd2e45fb75cf6
josiasapelete <josiasapelete@gmail.com>
2023-05-02 09:48:47 -0700

    Function that return the node of given index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,12 +11,12 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *temp = head;
 	unsigned int pos;
 
-	if ((head == NULL) || (pos > index))
+	if (head == NULL)
 		return (NULL);
 
-	listint_t *temp = head;
 
 	for (pos = 0; pos != index; pos++)
 	{
