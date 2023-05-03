63b77742a2743dd6f5ff02353ca2c597b7d74369
josiasapelete <josiasapelete@gmail.com>
2023-05-03 11:24:08 -0700

    arg to str

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,14 +22,20 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		/*calculate the length of all the args and +1 for the \n*/
-		len += strlen(av[i]) + 1;
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+		len++;
 	}
+	len++; /*for the '\n'*/
 
 	arr = malloc(sizeof(char) * len);
 
 	if (arr == NULL)
 		return (NULL);
 
+	pos = 0;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
