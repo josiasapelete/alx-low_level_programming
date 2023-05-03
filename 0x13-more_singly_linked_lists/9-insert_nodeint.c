5351be3d3d340e2eb71edba9ad3f18e6b9676f9c
josiasapelete <josiasapelete@gmail.com>
2023-05-02 18:34:52 -0700

    insert node at given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,101 +5,49 @@
 
 
 /**
- * CreateNewNode - function taht create new Node
- * @next: the pointer too the next element
- * @data: the data of the node
+ * insert_nodeint_at_index - function taht create new Node
+ * @head: the list
+ * @n: the data of the node
+ * @idx: the position
  * Return: the address of the node
  */
 
-listint_t *CreateNewNode(listint_t *next, int data)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newNode = malloc(sizeof(listint_t));
+	unsigned int pos;
+	listint_t *tmp, *newNode;
+
+	if (head == NULL)
+		return (NULL);
+
+
+	newNode = malloc(sizeof(listint_t));
 
 	if (newNode == NULL)
 		return (NULL);
 
-	newNode->n = data;
-	newNode->next = next;
-
-	return (newNode);
-}
-
-/**
- * push - function that inserts a node at the end
- * @head: the list
- * @n: the data
- * Return:return the list
- */
-
-listint_t *push(listint_t **head, int n)
-{
-	listint_t *newNode = CreateNewNode(NULL, n);
-	listint_t *temp = *head;
+	newNode->n = n;
 
 
-	if (*head == NULL)
+	if (idx == 0)
 	{
+		newNode->next = *head;
 		*head = newNode;
 		return (newNode);
 	}
 
+	if (idx != 0)
+	{
+		tmp = *head;
 
-	while (temp->next != NULL)
-		temp = (temp)->next;
+		for (pos = 0; pos < idx - 1 && tmp != NULL; pos++)
+			tmp = tmp->next;
 
-	(temp)->next = newNode;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	newNode->next = tmp->next;
+	tmp->next = newNode;
 	return (newNode);
-
-}
-/**
- * insert_nodeint_at_index - function that inserts a node at a given index
- * @head: the list
- * @idx: the position
- * @n: the data
- * Return: the new node
- */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-{
-
-	listint_t *tmp, *prec;
-	unsigned int len, pos;
-
-	tmp = *head;
-	len = 0;
-
-	if (head == NULL)
-		return (NULL);
-	while (tmp)
-	{
-	len++;
-	tmp = (tmp)->next;
-	}
-	if (idx == 0)
-	{
-	*head = CreateNewNode(*head, n);
-	return (*head);
-	}
-
-
-	if (idx == len)
-	{
-		return (push(head, n));
-	}
-
-	if (idx > len)
-		return (NULL);
-
-	prec = *head;
-	tmp = *head;
-
-	for (pos = 0; pos < idx; pos++)
-	{
-		prec = tmp;
-		tmp = tmp->next;
-	}
-
-	prec->next = CreateNewNode(tmp, n);
-
-	return (*head);
-
 }
