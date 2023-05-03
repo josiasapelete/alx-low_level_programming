c16e75853f4bf02bfecec6dd06ac9b06835211ae
josiasapelete <josiasapelete@gmail.com>
2023-05-03 10:55:40 -0700

    arg to str

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenate all the program args
+ * @ac: point to argc
+ * @av: point to argv
+ * Return: NULL if ac == 0 or av == NULL ELSE pointer to a new str
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int len, pos, i, j;
+	char *arr = NULL;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		/*calculate the length of all the args and +1 for the \n*/
+		len += strlen(av[i]) + 1;
+	}
+
+	arr = malloc(sizeof(char) * len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			arr[pos] = av[i][j];
+		       pos++;
+		}
+		arr[pos] = '\n';
+		pos++;
+
+	}
+
+	arr[pos] = '\n';
+	return (arr);
+
+
+}
