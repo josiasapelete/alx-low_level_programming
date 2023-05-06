cc39857f02b71ee2093cedcb7c5a3b0f7e71405b
josiasapelete <josiasapelete@gmail.com>
2023-05-05 17:37:35 -0700

    bit map binary_to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,9 +16,10 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
+	i = 0;
 	while (b[i] != '\0')
 	{
-	if (b[i] != '0' || b[i] != '1')
+	if (b[i] != '0' && b[i] != '1')
 		return (0);
 	i++;
 	}
