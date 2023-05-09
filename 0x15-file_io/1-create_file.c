267d86c809b6d486e7a203f7c471f564bf5f75a8
josiasapelete <josiasapelete@gmail.com>
2023-05-08 18:39:22 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, len;
+	int file, w, len;
 
 
 	if (filename == NULL)
@@ -27,9 +27,9 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (file == -1)
-		return (-1);
-	if ((write(file, text_content, len)) == -1)
+	w = (write(file, text_content, len));
+
+	if (w == -1 || file == -1)
 	{
 		close(file);
 		return (-1);
