5010606c8b292efde4bc30880dd20af4b17c151a
josiasapelete <josiasapelete@gmail.com>
2023-05-08 18:17:38 -0700

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 
 	}
 
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
