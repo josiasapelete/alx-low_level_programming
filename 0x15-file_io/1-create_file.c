76e54e11a275ef86d50ab1c1495ae586df8c983b
josiasapelete <josiasapelete@gmail.com>
2023-05-08 19:38:30 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,11 +23,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		close(file);
-		return(1);
+		return (1);
 	}
-	for (len = 0; text_content[len];len++);
 
-	
+
+	for (len = 0; text_content[len];)
+		len++;
+
 	w = write(file, text_content, len);
 
 	if (w == -1 || file == -1)
