72393e8b6404d0bedb375b0fa21fea4362ad7272
josiasapelete <josiasapelete@gmail.com>
2023-05-12 11:08:48 -0700

    test

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.>
+#include "main.h"
+
+
+/**
+ * main - function that copies file and paste it in another file
+ * @argc: the number of args
+ * @argv: contain all the args names
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *file_from, *file_to;
+	int file1, file2, len1, len2;
+
+
+	if (argc != 2)
+	{
+		printf("cp %s %s\n", argv[1], argv[2]);
+		exit(97);
+	}
+
+	file1 = open(argv[1], O_RDONLY);
+
+	if (file1 == -1)
+	{
+		printf("Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC);
+
+	if (file2 == -1)
+	{
+		printf("Can't write to %s \n", argv[2]);
+		exit(99);
+	}
+}
